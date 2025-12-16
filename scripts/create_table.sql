38a1b4b807c75db1d42048cb118e0cf2996db3c0
WhiteW00lf <mocha_cookie_crumble@proton.me>
2025-12-16 23:14:26 +0530

    created relevant tables

--- scripts/create_table.sql
+++ scripts/create_table.sql
@@ -1,8 +1,8 @@
 
 drop TABLE IF EXISTS customers;
 CREATE TABLE customers (
-    customer_id SERIAL PRIMARY KEY,
-    emp_title VARCHAR(100) NOT NULL,
+    customer_id  INT PRIMARY KEY,
+    emp_title VARCHAR(100),
     emp_length VARCHAR(50) NOT NULL,
     home_ownership VARCHAR(50) NOT NULL,
     annual_inc FLOAT NOT NULL,
@@ -11,6 +11,37 @@ CREATE TABLE customers (
     zip_code VARCHAR(20) NOT NULL,
     addr_state varchar(50),
     avg_cur_bal FLOAT,
-    tot_cur_bal INT
+    tot_cur_bal NUMERIC
 
+);
+
+DROP TABLE IF EXISTS countofloans;
+CREATE TABLE countofloans (
+    issue_year FLOAT,
+    loan_count INT
+);
+DROP TABLE IF EXISTS loan_purposes;
+CREATE TABLE loan_purposes (
+    purpose VARCHAR(100)
+);
+
+DROP TABLE IF EXISTS loans;
+CREATE TABLE loans (
+    loan_id INT PRIMARY KEY,
+    customer_id INT,
+    loan_status VARCHAR(50),
+    loan_amount DECIMAL(15,2),
+    state VARCHAR(50),
+    funded_amount DECIMAL(15,2),
+    term VARCHAR(20),
+    int_rate DECIMAL(5,4),
+    installment DECIMAL(10,2),
+    grade VARCHAR(10),
+    issue_date DATE,
+    issue_year INT,
+    pymnt_plan VARCHAR(10),
+    type VARCHAR(50),
+    purpose VARCHAR(100),
+    description TEXT,
+    notes TEXT
 );
